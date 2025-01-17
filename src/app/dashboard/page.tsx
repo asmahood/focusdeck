685c648f3b6e8c42a23b1efd6e23dac5e16aebdd
Adam Mahood <mahood.adam@gmail.com>
2025-01-17 18:44:48 -0500

    feat: add graphql client

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,16 +1,54 @@
+import { gql } from "@/__generated__";
 import { auth } from "@/auth"
+import { client } from "@/graphql/client";
 import { redirect } from "next/navigation";
+
+const findFirst20IssuesQuery = gql(`
+  query GetMostRecentIssues($owner: String!, $name: String!) {
+    repository(owner:$owner, name:$name) {
+      issues(last:20, states:CLOSED) {
+        edges {
+          node {
+            title
+            url
+            labels(first:5) {
+              edges {
+                node {
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }    
+  }
+`);
 
 export default async function DashboardPage() {
   const session = await auth();
 
   if (!session?.user || session.error === "RefreshTokenError") redirect("/sign-in");
 
+  const result = await client.query({
+    query: findFirst20IssuesQuery,
+    variables: {
+      owner: "uwpokerclub",
+      name: "app"
+    }
+  });
+
+  console.log(result.data.repository?.issues.edges)
+
   return (
     <div>
       <h1>Dashboard page</h1>
 
       <p>Hello {session.user.name}</p>
+
+      {result.data.repository?.issues.edges?.map((issue) => (
+        <p key={issue?.node?.url}>{issue?.node?.title || "No Results"}</p>
+      )) || <p>No Results</p>}
     </div>
   )
 }
---
+++ src/graphql/client.ts
@@ -0,0 +1,27 @@
+import { auth } from "@/auth";
+import { ApolloClient, createHttpLink, InMemoryCache } from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+  uri: "https://api.github.com/graphql",
+});
+
+const authLink = setContext(async (_, { headers }) => {
+  const session = await auth();
+  
+  const token = session?.user.accessToken ?? "";
+
+  return {
+    headers: {
+      ...headers,
+      authorization: `bearer ${token}`
+    }
+  }
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export { client };
